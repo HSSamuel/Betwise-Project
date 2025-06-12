32c4edcf72e646fd82416684d9c7d3871bb027d4
Hunsa Semako S. <smkmayomisamuel@gmail.com>
2025-06-12 05:33:57 +0100

    updated

--- cli/seedGames.js
+++ cli/seedGames.js
@@ -43,7 +43,7 @@ const sampleGames = [
     awayTeam: "Inter Miami",
     odds: { home: 2.5, away: 2.8, draw: 3.4 },
     league: "Cub World Cup",
-    matchDate: inThreeDays.setHours(13, 0, 0), // In three days at 1:00 PM
+    matchDate: inTwoDays.setHours(13, 0, 0), // In two days at 1:00 PM
     status: "upcoming",
   },
   {
@@ -51,7 +51,7 @@ const sampleGames = [
     awayTeam: "Auckland City",
     odds: { home: 1.9, away: 3.5, draw: 3.8 },
     league: "Cub World Cup",
-    matchDate: inThreeDays.setHours(17, 0, 0), // Tomorrow at 5:00 PM
+    matchDate: inTwoDays.setHours(17, 0, 0), // In two days at 5:00 PM
     status: "upcoming",
   },
   {
@@ -59,7 +59,7 @@ const sampleGames = [
     awayTeam: "Atletico Madrid",
     odds: { home: 1.5, away: 5.5, draw: 4.5 },
     league: "Club World Cup",
-    matchDate: inThreeDays.setHours(20, 30, 0), // In two days at 8:00 PM
+    matchDate: inTwoDays.setHours(20, 30, 0), // In two days at 8:00 PM
     status: "upcoming",
   },
   {
@@ -67,7 +67,7 @@ const sampleGames = [
     awayTeam: "Porto",
     odds: { home: 2.2, away: 3.1, draw: 2.9 },
     league: "Club World Cup",
-    matchDate: inThreeDays.setHours(23, 0, 0), // In two days at 9:00 PM
+    matchDate: inTwoDays.setHours(23, 0, 0), // In two days at 9:00 PM
     status: "upcoming",
   },
 ];
